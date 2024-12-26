693b0334e9c0d3f95111ec7f4a9f67587dbcffff
jiangyuan <joe469391363@gmail.com>
2024-12-26 21:56:41 +0800

    update

--- x-rpc-api/src/main/java/io/github/xinfra/lab/rpc/common/ClassUtils.java
+++ x-rpc-api/src/main/java/io/github/xinfra/lab/rpc/common/ClassUtils.java
@@ -22,28 +22,6 @@ import java.util.stream.Collectors;
 
 public class ClassUtils {
 
-  public static Object getDefaultPrimitiveValue(Class<?> clazz) {
-    if (clazz == int.class) {
-      return 0;
-    } else if (clazz == boolean.class) {
-      return false;
-    } else if (clazz == long.class) {
-      return 0L;
-    } else if (clazz == byte.class) {
-      return (byte) 0;
-    } else if (clazz == double.class) {
-      return 0d;
-    } else if (clazz == short.class) {
-      return (short) 0;
-    } else if (clazz == float.class) {
-      return 0f;
-    } else if (clazz == char.class) {
-      return (char) 0;
-    } else {
-      return null;
-    }
-  }
-
   public static String genMethodSign(Method method) {
     return method.getName()
         + "("
